01d6e3d9a5f3ca1113086a064fe4a9dc2d4f76d2
Vahan11 <vahansargsyan1990@gmail.com>
2021-06-04 16:28:36 +0400

    feat: message to Satenik

---
+++ anotherPr.js
@@ -0,0 +1,10 @@
+"use strict";
+
+function sendMessage(message) {
+  console.log(message);
+}
+
+let message =
+  "Dear Satenik, please, do not hurry, make the code review of my best code :)";
+
+sendMessage(message);
